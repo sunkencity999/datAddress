7c44209c4c95b7f6135fd565171e940357bd04cd
sunkencity999 <theroyalart@gmail.com>
2015-11-16 13:46:34 -0800

    refined greeting script

--- greeting.rb
+++ greeting.rb
@@ -1,7 +1,8 @@
 
 def greeting
+  greeting = ARGV.shift
   ARGV.each do |arg|
-  puts "Hello #{arg}! Thank you for using datAddress. We aim to be a unique and simple solution for personally cultivating contacts. The people you add to datAddress are the people who *matter*.--Christopher "
+  puts " #{greeting} #{arg}! Thank you for using datAddress. We aim to be a unique and simple solution for personally cultivating contacts. The people you add to datAddress are the people who *matter*.--Christopher "
   end
 end
 
