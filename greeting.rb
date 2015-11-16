f2a7f956498f661b4f42cebf9c92447462929927
sunkencity999 <theroyalart@gmail.com>
2015-11-16 11:48:12 -0800

    Added greeting script

---
+++ greeting.rb
@@ -0,0 +1,8 @@
+
+def greeting
+  ARGV.each do |arg|
+  puts "Hello #{arg}! Thank you for using datAddress. We aim to be a unique and simple solution for personally cultivating contacts. The people you add to datAddress are the people who *matter*.--Christopher "
+  end
+end
+
+greeting
